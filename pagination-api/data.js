8896d980ad41c4df10b7cb2eeabf52b69af2fbd1
AGNES LILY <142089703+agneslily4709@users.noreply.github.com>
2023-11-02 22:31:32 +0530

    Update data.js

--- pagination-api/data.js
+++ pagination-api/data.js
@@ -12,3 +12,18 @@ export const users = [
         { id:11, name:"User 11"},
         { id:12, name:"User 12"},
 ]
+
+export const posts = [
+        { id:1, name:"Post 1"},
+        { id:2, name:"Post 2"},
+        { id:3, name:"Post 3"},
+        { id:4, name:"Post 4"},
+        { id:5, name:"Post 5"},
+        { id:6, name:"Post 6"},
+        { id:7, name:"Post 7"},
+        { id:8, name:"Post 8"},
+        { id:9, name:"Post 9"},
+        { id:10, name:"Post 10"},
+        { id:11, name:"Post 11"},
+        { id:12, name:"Post 12"},
+]
